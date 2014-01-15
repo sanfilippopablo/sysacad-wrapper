2908897c1e0ed8041f9ea71047bc25c8380a7cba
Pablo Sanfilippo <giamgreeg@gmail.com>
2014-01-15 19:25:01 -0300

    Agregada la opción ?cached=false a /api/alumno/:pk/ para obtener datos directamente desde Sysacad (y cachearlos).

--- backend/website/api_views.py
+++ backend/website/api_views.py
@@ -1,8 +1,9 @@
-from rest_framework import views, generics
+from rest_framework import views, generics, exceptions, serializers
 from rest_framework.permissions import BasePermission
 from django.contrib.auth import authenticate, login, logout
 from rest_framework.serializers import ModelSerializer
 from rest_framework.response import Response
+from website.auth import SysacadSession
 
 from website.models import Alumno
 
@@ -20,7 +21,7 @@ class AlumnoSerializer(ModelSerializer):
         model = Alumno
         fields = ('id', 'first_name', 'last_name', 'fr', 'legajo', 'email')
         read_only_fields = ('id', 'first_name', 'last_name', 'fr', 'legajo')
-        write_only_fields = ('password',)
+        write_only_fields = ('password', 'old_password')
 
     def restore_object(self, attrs, instance=None):
         assert instance is not None, 'Solo updates.'
@@ -57,6 +58,21 @@ class Logout(views.APIView):
             return Response({}, 401)
 
 class AlumnosDetail(generics.RetrieveUpdateAPIView):
+
+    def get_object(self):
+        alumno = super(AlumnosDetail, self).get_object()
+        if self.request.QUERY_PARAMS.get('cached', 'true') == 'false':
+            # Actualizar la data del alumno
+            sysacad = SysacadSession(alumno=alumno)
+            try:
+                datos_alumno = sysacad.estado_academico_data()['datos_alumno']
+            except SysacadSession.AuthenticationError:
+                raise exceptions.NotAuthenticated
+            alumno.first_name, alumno.last_name = datos_alumno
+            alumno.save()
+            sysacad.close()
+
+        return alumno
 
     model = Alumno
     serializer_class = AlumnoSerializer
